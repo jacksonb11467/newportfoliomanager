b9a5fd3078a09c8a09e8beb08cd846da1cc087f6
Jackson Boehme <boehmejackson@gmail.com>
2025-11-02 14:55:40 +1000

    Add trust proxy for Railway

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const session = require('express-session');
 const { Resend } = require('resend');
 
 const app = express();
+app.set('trust proxy', 1);
 app.use(express.json());
 
 app.use(session({
